cd59da99ca15608bc47eed8c9ef85842a31c91e6
zoujingyi <zoujingyi1992@gmail.com>
2016-11-06 20:01:53 +0800

    commit

--- app/src/main/java/com/zou/autotouch/MainActivity.java
+++ app/src/main/java/com/zou/autotouch/MainActivity.java
@@ -3,14 +3,19 @@ package com.zou.autotouch;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ListView;
 import android.widget.Toast;
 
 import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = "MainActivity";
+    private ListView lv_main;
     private Button start,stop;
     private Button btn1;
     private VirtualTerminal vt;
@@ -23,6 +28,24 @@ public class MainActivity extends AppCompatActivity {
         setListener();
 
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.action_add:
+                Toast.makeText(this,"开始录制脚本",Toast.LENGTH_SHORT).show();
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
     private void initData() {
         try {
             vt = new VirtualTerminal("su");
@@ -34,6 +57,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void initView(){
+        lv_main = (ListView) findViewById(R.id.lv_main);
         start = (Button) findViewById(R.id.btn_start);
         stop = (Button) findViewById(R.id.btn_stop);
         btn1 = (Button) findViewById(R.id.btn1);
