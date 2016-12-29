2c7bf4d514de58f866eb2a4ba0565b4361012ae7
zou <zoujingyi1992@gmail.com>
2016-12-29 17:37:24 +0800

    核心功能测试

--- app/src/main/java/com/zou/autotouch/MainActivity.java
+++ app/src/main/java/com/zou/autotouch/MainActivity.java
@@ -1,6 +1,7 @@
 package com.zou.autotouch;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
+import android.os.ParcelFileDescriptor;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
@@ -10,20 +11,23 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.ListView;
 import android.widget.Toast;
+import java.io.File;
 
-import java.io.DataOutputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.OutputStream;
 import java.util.ArrayList;
 
-public class MainActivity extends AppCompatActivity {
+
+import jackpal.androidterm.TermExec;
+
+public class MainActivity extends Activity {
     private static final String TAG = "MainActivity";
     private ListView lv_main;
     private Button start,stop;
     private Button btn1;
-    private VirtualTerminal vt;
-    private ArrayList<String> cmdstr;
-    private DataOutputStream localOutputStream;
+    private ParcelFileDescriptor mTermFd;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -31,46 +35,132 @@ public class MainActivity extends AppCompatActivity {
         initData();
         initView();
         setListener();
-
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()){
-            case R.id.action_add:
-                Toast.makeText(this,"开始run脚本",Toast.LENGTH_SHORT).show();
-                try {
-                    for(int i =0;i<cmdstr.size();i++){
-                        String str = "sendevent "+cmdstr.get(i);
-                        Log.i(TAG,"str : "+str);
-                        vt.runCommand(str);
-//                        localOutputStream.writeBytes(str+ "\necho :RET=$?\n");
-//                        localOutputStream.flush();
-                    }
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-                break;
-        }
-        return super.onOptionsItemSelected(item);
     }
 
     private void initData() {
         try {
-            localOutputStream = new DataOutputStream(Runtime.getRuntime().exec("su").getOutputStream());
-            vt = new VirtualTerminal("su");
+            mTermFd = ParcelFileDescriptor.open(new File("/dev/ptmx"), ParcelFileDescriptor.MODE_READ_WRITE);
+            final InputStream in = new ParcelFileDescriptor.AutoCloseInputStream(mTermFd);
+            OutputStream out = new ParcelFileDescriptor.AutoCloseOutputStream(mTermFd);
+            initializeSession();
+            new Thread(){
+                @Override
+                public void run() {
+                    byte[] buffer = new byte[4096];
+                    while (true){
+                        try {
+                            int read = in.read(buffer);
+                            String str = new String(buffer,0,read);
+                            Log.i(TAG,"read: "+str);
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }
+            }.start();
+            out.write("su\n".getBytes());
+            out.flush();
+            out.write("getevent\n".getBytes());
+            out.flush();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
         } catch (IOException e) {
             e.printStackTrace();
-        } catch (InterruptedException e) {
-            e.printStackTrace();
         }
+    }
+
+    private void initializeSession() throws IOException {
+        String path = System.getenv("PATH");
+        path = checkPath(path);
+
+        String[] env = new String[3];
+        env[0] = "TERM=screen";
+        env[1] = "PATH=" + path;
+        env[2] = "HOME=/data/user/0/jackpal.androidterm/app_HOME";
+
+        int mProcId = createSubprocess("/system/bin/sh -", env);
+    }
+
+    private int createSubprocess(String shell, String[] env) throws IOException {
+        ArrayList<String> argList = parse(shell);
+        String arg0;
+        String[] args;
+
+        try {
+            arg0 = argList.get(0);
+            File file = new File(arg0);
+            if (!file.exists()) {
+                throw new FileNotFoundException(arg0);
+            } else if (!file.canExecute()) {
+                throw new FileNotFoundException(arg0);
+            }
+            args = argList.toArray(new String[1]);
+        } catch (Exception e) {
+            arg0 = argList.get(0);
+            args = argList.toArray(new String[1]);
+        }
+
+        return TermExec.createSubprocess(mTermFd, arg0, args, env);
+    }
+    private ArrayList<String> parse(String cmd) {
+        final int PLAIN = 0;
+        final int WHITESPACE = 1;
+        final int INQUOTE = 2;
+        int state = WHITESPACE;
+        ArrayList<String> result =  new ArrayList<String>();
+        int cmdLen = cmd.length();
+        StringBuilder builder = new StringBuilder();
+        for (int i = 0; i < cmdLen; i++) {
+            char c = cmd.charAt(i);
+            if (state == PLAIN) {
+                if (Character.isWhitespace(c)) {
+                    result.add(builder.toString());
+                    builder.delete(0,builder.length());
+                    state = WHITESPACE;
+                } else if (c == '"') {
+                    state = INQUOTE;
+                } else {
+                    builder.append(c);
+                }
+            } else if (state == WHITESPACE) {
+                if (Character.isWhitespace(c)) {
+                    // do nothing
+                } else if (c == '"') {
+                    state = INQUOTE;
+                } else {
+                    state = PLAIN;
+                    builder.append(c);
+                }
+            } else if (state == INQUOTE) {
+                if (c == '\\') {
+                    if (i + 1 < cmdLen) {
+                        i += 1;
+                        builder.append(cmd.charAt(i));
+                    }
+                } else if (c == '"') {
+                    state = PLAIN;
+                } else {
+                    builder.append(c);
+                }
+            }
+        }
+        if (builder.length() > 0) {
+            result.add(builder.toString());
+        }
+        return result;
+    }
+
+    private String checkPath(String path) {
+        String[] dirs = path.split(":");
+        StringBuilder checkedPath = new StringBuilder(path.length());
+        for (String dirname : dirs) {
+            File dir = new File(dirname);
+            if (dir.isDirectory() && dir.canExecute()) {
+                checkedPath.append(dirname);
+                checkedPath.append(":");
+            }
+        }
+        return checkedPath.substring(0, checkedPath.length()-1);
     }
 
     private void initView(){
@@ -127,43 +217,58 @@ public class MainActivity extends AppCompatActivity {
         stop.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                vt.shutdown();
                 Toast.makeText(getApplicationContext(),"stop onclick",Toast.LENGTH_SHORT).show();
-                String last_input=vt.getLastInputString();
-                String[] last_inputs = last_input.split("\n");
-                for(int i=0;i<last_inputs.length;i++){
-                    if(cmdstr == null){
-                        cmdstr = new ArrayList<String>();
-                    }
-                    if(last_inputs[i].startsWith("/dev/input/event")){
-                        cmdstr.add(last_inputs[i]);
-                        Log.i(TAG,"cmdstr : "+last_inputs[i]);
-                    }
-                }
+//                String last_input=vt.getLastInputString();
+//                String[] last_inputs = last_input.split("\n");
+//                for(int i=0;i<last_inputs.length;i++){
+//                    if(cmdstrs == null){
+//                        cmdstrs = new ArrayList<String>();
+//                    }
+//                    if(last_inputs[i].startsWith("/dev/input/event")){
+//                        String cmdstr = format(last_inputs[i]);
+//                        cmdstrs.add(cmdstr);
+//                        Log.i(TAG,"cmdstr : "+cmdstr);
+//                    }
+//                }
             }
         });
     }
 
-    //开始记录用户数据
-    private void recordEvent(){
-        new Thread(){
-            @Override
-            public void run() {
-                exeCommand("getevent");
+    private String format(String cmdstr){
+        StringBuffer newstr=new StringBuffer();
+        cmdstr.replace(":","");
+        String[] strs_16 =  cmdstr.split(" ");
+        for(int i=1;i<strs_16.length;i++){
+            if("ffffffff".equals(strs_16[i])){
+                strs_16[i]="-1";
+            }else {
+                strs_16[i] = Integer.parseInt(strs_16[i], 16) + "";
             }
-        }.start();
+        }
+        newstr.append("sendevent");
+        for(int i=0;i<strs_16.length;i++){
+            newstr = newstr.append(" "+strs_16[i]);
+        }
+        return newstr.toString();
     }
 
-    private boolean exeCommand(String command) {
-        boolean ret = false;
-        try {
-            VirtualTerminal.VTCommandResult r = vt.runCommand(command);
-            ret = r.success();
-            vt.shutdown();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
-        return ret;
+    //开始记录用户数据
+    private void recordEvent(){
+//        new Thread(){
+//            @Override
+//            public void run() {
+//                try {
+//                    Command command = new Command(0,"getevent");
+//                    RootTools.getShell(true).add(command);
+//                } catch (IOException e) {
+//                    e.printStackTrace();
+//                } catch (TimeoutException e) {
+//                    e.printStackTrace();
+//                } catch (RootDeniedException e) {
+//                    e.printStackTrace();
+//                }
+//            }
+//
+//        }.start();
     }
 }
